8da50f5d370b91b19155f04551b4c475e7dbbdec
Frank Blechschmidt <contact@frank-blechschmidt.com>
2019-08-04 22:58:12 -0700

    Bundle NoUISlider

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import "core-js/stable"
-import "regenerator-runtime/runtime"
+import "core-js/stable";
+import "regenerator-runtime/runtime";
 
-import Slideshow from "./slideshow.js"
-import "./styles/app.css"
+import Slideshow from "./slideshow";
+import "./nouislider";
+import "./styles/app.css";
 
 document.addEventListener("DOMContentLoaded", async () => {
-    const slideshow = new Slideshow()
-    await slideshow.initialize()
-    await slideshow.enable()
+  const slideshow = new Slideshow();
+  await slideshow.initialize();
+  await slideshow.enable();
 });
